0dd1657134570b7b9c187c7f5bb50f4f42a44479
Charles Helmich <charlie.helmich@gmail.com>
2018-11-05 18:54:51 -0700

    Move sync pulses after pixels to simplify timing

--- src/hdmi_sync.vhd
+++ src/hdmi_sync.vhd
@@ -16,19 +16,19 @@ end hdmi_sync;
 
 architecture rtl of hdmi_sync is
     -- 640x480 60hz
+    constant HRES_LEN : integer := 640; -- usable pixels
     constant HFP_LEN : integer := 16; -- front porch
     constant HSYNC_LEN : integer := 96; -- sync pulse
     constant HBP_LEN : integer := 48; -- back porch
-    constant HRES_LEN : integer := 640; -- usable pixels
     constant HBLANK_LEN : integer := HFP_LEN + HSYNC_LEN + HBP_LEN;
-    constant HTOTAL_LEN : integer := HBLANK_LEN + HRES_LEN;
+    constant HTOTAL_LEN : integer := HRES_LEN + HBLANK_LEN;
 
+    constant VRES_LEN : integer := 480; -- usable pixels
     constant VFP_LEN : integer := 11; -- front porch
     constant VSYNC_LEN : integer := 2; -- sync pulse
     constant VBP_LEN : integer := 31; -- back porch
-    constant VRES_LEN : integer := 480; -- usable pixels
     constant VBLANK_LEN : integer := VFP_LEN + VSYNC_LEN + VBP_LEN;
-    constant VTOTAL_LEN : integer := VBLANK_LEN + VRES_LEN;
+    constant VTOTAL_LEN : integer := VRES_LEN + VBLANK_LEN;
 
     -- registers
     signal hcount_reg, hcount_next : unsigned(10 downto 0) := (others=>'0');
@@ -49,19 +49,30 @@ begin
     end process;
 
     -- output pixel coordinate signals
-    pixel_x <= hcount_reg - HBLANK_LEN when hcount_reg >= HBLANK_LEN
-               else (others=>'0');
-    pixel_y <= vcount_reg - VBLANK_LEN when vcount_reg >= VBLANK_LEN
-               else (others=>'0');
+    pixel_x <= hcount_reg when hcount_reg < HRES_LEN else
+               (others=>'0');
+    pixel_y <= vcount_reg when vcount_reg < VRES_LEN else
+               (others=>'0');
+
+    -- video enable
+    hdmi_enable <= '1' when (hcount_reg < HRES_LEN) and
+                            (vcount_reg < VRES_LEN) else
+                   '0';
+
+    -- horizontal sync pulse
+    hdmi_hsync <= '0' when (hcount_reg > HRES_LEN + HFP_LEN - 1) and
+                           (hcount_reg < HRES_LEN + HFP_LEN + HSYNC_LEN) else
+                  '1';
+
+    -- vertical sync pulse
+    hdmi_vsync <= '0' when (vcount_reg > VRES_LEN + VFP_LEN - 1) and
+                           (vcount_reg < VRES_LEN + VFP_LEN + VSYNC_LEN) else
+                  '1';
 
     -- combinational block
     process (hcount_reg, vcount_reg)
     begin
         -- default values
-        hdmi_enable <= '0';
-        hdmi_hsync <= '1';
-        hdmi_vsync <= '1';
-
         vcount_next <= (others => '0');
         hcount_next <= (others => '0');
 
@@ -76,21 +87,6 @@ begin
         else
             hcount_next <= hcount_reg + 1;
             vcount_next <= vcount_reg;
-        end if;
-
-        -- video enable
-        if (vcount_reg > VBLANK_LEN - 1) and (hcount_reg > HBLANK_LEN - 1) then
-            hdmi_enable <= '1';
-        end if;
-
-        -- horizontal sync pulse
-        if (hcount_reg > HFP_LEN - 1) and (hcount_reg < HFP_LEN + HSYNC_LEN) then
-            hdmi_hsync <= '0';
-        end if;
-
-        -- vertical sync pulse
-        if (vcount_reg > VFP_LEN - 1) and (vcount_reg < VFP_LEN + VSYNC_LEN) then
-            hdmi_vsync <= '0';
         end if;
     end process;
 end rtl;
